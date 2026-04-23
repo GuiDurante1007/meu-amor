ffd81656d95a98e60c8b16ed7f70afe593957e85
Guilherme Durante <rxxnyt8xrj@privaterelay.appleid.com>
2026-04-23 19:55:22 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+       alert('Boas vindas ao jogo do número secreto');
+        //parseint tira as casas decimais do Math.random
+        let numeroSecreto = parseInt(Math.random() * 100  + 1);
+        console.log(numeroSecreto);
+        let chute;
+        let tentativas = 1;
+
+        //enquanto o chute for diferente do numerosecreto, continua
+        while(chute != numeroSecreto){
+            chute = prompt('escolha um numero entre 1 e 100');
+        // se chute for igual ao número secreto
+        if (chute == numeroSecreto) {
+            break;
+        } else {
+            if(chute > numeroSecreto){
+                alert(`o numero secreto é menor que ${chute}`);
+            }
+                else{
+                    alert(`o numero secreto é maior que ${chute}`);
+                }
+                tentativas++;
+            }
+        }       
+
+        let pluralsingular = tentativas > 1 ? 'tentativas' : 'tentativa';
+         alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${pluralsingular}`);
+       // if(tentativas > 1){
+        // alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        //   }
+        //   else{
+         //    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+        //   }
